05cfed6030d98c35cf7c9592b9a10eea7b62e9ff
tateisnoone <tatdagelo@gmail.com>
2026-03-30 23:29:12 +0200

    testing pipeline

--- src/test/java/ge/tbc/testautomation/tests/TestTest.java
+++ src/test/java/ge/tbc/testautomation/tests/TestTest.java
@@ -3,6 +3,7 @@ package ge.tbc.testautomation.tests;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 @Epic("API Integration")
@@ -13,6 +14,10 @@ public class TestTest extends BaseTest {
     @Description("Validate CTA title from API matches UI.")
     public void validateCtaTitleApiVsUi() {
        commonSteps.goToHome();
+    }
+    @Test(priority = 2)
+    public void failureTest() {
+        Assert.assertTrue(false, "Test failure for Allure demo");
     }
 
 
