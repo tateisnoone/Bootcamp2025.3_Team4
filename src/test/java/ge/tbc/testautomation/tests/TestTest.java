eeb053156d9955218d24eddd5b76ec9fb4a5ec21
tateisnoone <tatdagelo@gmail.com>
2026-03-30 23:16:36 +0200

    testing pipeline

--- src/test/java/ge/tbc/testautomation/tests/TestTest.java
+++ src/test/java/ge/tbc/testautomation/tests/TestTest.java
@@ -1,56 +1,19 @@
 package ge.tbc.testautomation.tests;
 
-import ge.tbc.testautomation.models.apiResponse.PageResponse;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import static ge.tbc.testautomation.Constants.VALID_PAGE_ID;
 
 @Epic("API Integration")
 @Feature("Cards Page Validation")
 public class TestTest extends BaseTest {
-    private PageResponse pageResponse;
-
-    @BeforeClass
-    public void setUpApiResponse() {
-        pageResponse = apiSteps.getPage(VALID_PAGE_ID);
-    }
 
     @Test(priority = 1)
     @Description("Validate CTA title from API matches UI.")
     public void validateCtaTitleApiVsUi() {
-        apiSteps.validateCtaTitle(pageResponse, cardsSteps.getMainTitle());
+       commonSteps.goToHome();
     }
 
-    @Test(priority = 2)
-    @Description("Validate CTA button from API matches UI.")
-    public void validateCtaButtonApiVsUi() {
-        apiSteps.validateCtaButton(pageResponse, cardsSteps.getApplyButtonText());
-    }
 
-    @Test(priority = 3)
-    @Description("Validate CTA list from API matches UI.")
-    public void validateCtaListApiVsUi() {
-        apiSteps.validateCtaList(pageResponse, cardsSteps.getCtaList());
-    }
-
-    @Test(priority = 4)
-    @Description("Validate cards grid from API matches UI.")
-    public void validateCardsApiVsUi() {
-        apiSteps.validateCardsContent(pageResponse, cardsSteps.getAllCardTitles());
-    }
-
-    @Test(priority = 5)
-    @Description("Validate old cards from API matches UI.")
-    public void validateOldCardsApiVsUi() {
-        apiSteps.validateOldCardsContent(pageResponse, cardsSteps.getOldCardTitles());
-    }
-
-    @Test(priority = 6)
-    @Description("Validate banner title from API matches UI.")
-    public void validateBannerTitleApiVsUi() {
-        apiSteps.validateBannerTitle(pageResponse, cardsSteps.getBannerTitle());
-    }
 }
