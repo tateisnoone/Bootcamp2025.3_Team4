e390880d87912f479029b26678165fbc8cd14eba
tateisnoone <tatdagelo@gmail.com>
2026-04-04 17:05:42 +0200

    fix for pipeline

--- src/test/java/ge/tbc/testautomation/tests/CardsApiUiTests.java
+++ src/test/java/ge/tbc/testautomation/tests/CardsApiUiTests.java
@@ -1,6 +1,7 @@
 package ge.tbc.testautomation.tests;
 
 import ge.tbc.testautomation.models.apiResponse.PageResponse;
+import ge.tbc.testautomation.utils.RetryCount;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
@@ -11,6 +12,7 @@ import static ge.tbc.testautomation.Constants.VALID_PAGE_ID;
 @Epic("API Integration")
 @Feature("Cards Page")
 @Test(groups = {"E2E - Details from API matches displayed information - T4-T4"})
+@RetryCount(count = 2)
 public class CardsApiUiTests extends BaseTest {
     private PageResponse pageResponse;
 
